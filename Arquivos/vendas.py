6dd78503b4363c131fd892861241ea2fd7aa5e96
flushedlucas <lucas.vieira.silva@live.com>
2017-02-15 22:54:20 -0300

    relatório de vendas

--- Arquivos/vendas.py
+++ Arquivos/vendas.py
@@ -8,21 +8,29 @@ def relatorio():
             """)
     opcao = input("Digite a opcao: ")
     if opcao == str(1):
-        arq = open("vendas.txt")
-        linhas = arq.readlines()
-        arq.close()
-        for i,j in enumerate(linhas):
-            linhas[i] = j.split(",")
-        for i, j in enumerate(linhas):
-            pass
+        cliente = input("Digite o nome do cliente: ")
+        if cliente != None:
+            arq = open("vendas.txt")
+            linhas = arq.readlines()
+            arq.close()
+            for i,j in enumerate(linhas):
+                linhas[i] = j.split(",")
+            print("\n" + cliente)
+            for i, j in enumerate(linhas):
+                if j[0] == cliente:
+                    print("Comprou %s de %s do vendedor %s" %(j[3],j[2],j[1]))
 
     elif opcao == str(2):
-        arq = open("vendas.txt")
-        linhas = arq.readlines()
-        arq.close()
-        for i, j in enumerate(linhas):
-            linhas[i] = j.split(",")
-        for i, j in enumerate(linhas):
-            pass
+        vendedor = input("Digite o nome do vendedor: ")
+        if vendedor != None:
+            arq = open("vendas.txt")
+            linhas = arq.readlines()
+            arq.close()
+            for i, j in enumerate(linhas):
+                linhas[i] = j.split(",")
+            print("\n" + vendedor)
+            for i, j in enumerate(linhas):
+                if j[1] == vendedor:
+                    print("Vendeu %s de %s para o cliente %s" % (j[3], j[2], j[0]))
     else:
         print("Opcao Invalida")
